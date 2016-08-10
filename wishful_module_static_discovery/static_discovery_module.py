d653c748d2b87a98cc8e00d09c33d44a9ccc240b
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-08-10 13:55:10 +0200

    discovery is module now and produces events

--- wishful_module_static_discovery/static_discovery_module.py
+++ wishful_module_static_discovery/static_discovery_module.py
@@ -1,7 +1,7 @@
 import logging
 import time
-import random
 import wishful_framework
+import wishful_upis as upis
 
 __author__ = "Piotr Gawlowicz"
 __copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
@@ -18,27 +18,24 @@ class StaticDiscoveryModule(wishful_framework.WishfulModule):
         self.controller_dl = downlink
         self.controller_ul = uplink
 
-
     @wishful_framework.run_in_thread()
     @wishful_framework.on_start()
     @wishful_framework.on_disconnected()
     def start_discovery(self):
         self.log.debug("Start static discovery procedure".format())
         self.running = True
-
         while self.running:
-            #do nothing
             time.sleep(2)
-
+            self.log.debug("Discovered Controller DL-{}, UL-{}"
+                           .format(self.controller_dl,
+                                   self.controller_ul))
+            self.send_event(
+                upis.mgmt.ControllerDiscoveredEvent(
+                    self.controller_dl, self.controller_ul)
+            )
 
     @wishful_framework.on_exit()
     @wishful_framework.on_connected()
     def stop_discovery(self):
         self.log.debug("Stop static discovery procedure".format())
         self.running = False
-
-
-    @wishful_framework.discover_controller()
-    def get_controller(self):
-        self.log.debug("Get Controller addresses: DL:{}, UL:{}".format(self.controller_dl, self.controller_ul))
-        return [self.controller_dl, self.controller_ul]
